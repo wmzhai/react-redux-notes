a60bc28bde248268b4528d88f10ea828fed1a3db
Weiming Zhai <wmzhai@gmail.com>
2017-02-26 16:35:06 +0800

    add SearchBar

--- projects/proj1/src/index.js
+++ projects/proj1/src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCrsUUKgGOrOAQXVu0y6c5sImWrPUwPt98';
 
 const App = function() {
-  return <div>Hi!</div>;
+  return ( 
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render( <App />, document.querySelector('.container'));
