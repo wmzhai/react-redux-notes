57ee0ca1631983a10cf1cf8d53ff3439b469cf8b
Weiming Zhai <wmzhai@gmail.com>
2017-02-26 21:49:15 +0800

    youtube search

--- projects/proj1/src/index.js
+++ projects/proj1/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCrsUUKgGOrOAQXVu0y6c5sImWrPUwPt98';
+
+YTSearch({key: API_KEY, term: 'surfboards'},function(data){
+  console.log(data);
+});
 
 const App = function() {
   return ( 
