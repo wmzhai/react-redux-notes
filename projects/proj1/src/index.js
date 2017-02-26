ba6af9cc61b6f63a6d0b96bf81876b79178a1e23
Weiming Zhai <wmzhai@gmail.com>
2017-02-26 22:00:37 +0800

    functional component -> class component

--- projects/proj1/src/index.js
+++ projects/proj1/src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 const API_KEY = 'AIzaSyCrsUUKgGOrOAQXVu0y6c5sImWrPUwPt98';
 
-YTSearch({key: API_KEY, term: 'surfboards'},function(data){
-  console.log(data);
-});
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const App = function() {
-  return ( 
-    <div>
-      <SearchBar />
-    </div>
-  );
+    this.state = {
+      videos: []
+    };
+    YTSearch({key: API_KEY,term: 'surfboards'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar/>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render( <App />, document.querySelector('.container'));
+ReactDOM.render(
+  <App/>, document.querySelector('.container'));
