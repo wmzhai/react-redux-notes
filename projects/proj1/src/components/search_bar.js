c6432f1d6cdb755b0b9b4cae853f0f76ab994860
Weiming Zhai <wmzhai@gmail.com>
2017-02-26 17:45:59 +0800

    setState when input onChange

--- projects/proj1/src/components/search_bar.js
+++ projects/proj1/src/components/search_bar.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: ''};
+  }
+
   render() {
-    return <input onChange={e => console.log(e.target.value)}/>;
+    return (
+      <div>
+        <input onChange={e => this.setState({term: e.target.value})}/>
+      </div>
+      );
   }
 
 }
