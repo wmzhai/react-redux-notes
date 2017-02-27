5846391f00e5e64574f7ac3b84920821141a877d
Weiming Zhai <wmzhai@gmail.com>
2017-02-27 19:53:45 +0800

    ajax request

--- projects/proj3/src/actions/index.js
+++ projects/proj3/src/actions/index.js
@@ -1,2 +1,16 @@
-const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
+import axios from 'axios';
 
+const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
