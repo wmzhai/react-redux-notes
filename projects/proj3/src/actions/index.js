6af5a4878fdb332827905a504f72b2fdb847c004
Weiming Zhai <wmzhai@gmail.com>
2017-02-28 11:57:21 +0800

    request weather

--- projects/proj3/src/actions/index.js
+++ projects/proj3/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'b1b15e88fa797225412429c1c50c122a1';
+const API_KEY = 'd3f64acf736bdaa54816a13e1ffd71ec';
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
@@ -8,6 +8,8 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+
+  console.log('Request:', request);
 
   return {
     type: FETCH_WEATHER,
--- projects/proj3/src/reducers/index.js
+++ projects/proj3/src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import WeathReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeathReducer
 });
 
 export default rootReducer;
