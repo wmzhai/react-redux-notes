90b2411e398a2eab67594c2b41bf9cffe8c865e1
Weiming Zhai <wmzhai@gmail.com>
2017-03-06 23:26:36 +0800

    finish all testing

--- projects/testing/test/test_helper.js
+++ projects/testing/test/test_helper.js
@@ -1,36 +1,42 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
+import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils';
+import ReactDOM from 'react-dom';
+import chai,{ expect } from 'chai';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
+// 1. set up testing environment to run like a browser in the command line
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = jquery(global.window);
 
-chaiJquery(chai, chai.util, $);
-
-function renderComponent(ComponentClass, props = {}, state = {}) {
+// 2. build 'renderComponent' helper that should render a given react ComponentClass
+function renderComponent(ComponentClass, props, state) {
   const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
+    <Provider store={createStore(reducers,state)} >  
+      <ComponentClass {...props}/>
     </Provider>
-  );
+    );
 
   return $(ReactDOM.findDOMNode(componentInstance));
 }
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
+// 3. build helper for simulating events
+$.fn.simulate = function(eventName, value){
+  if(value) {
     this.val(value);
   }
-  TestUtils.Simulate[eventName](this[0]);
-};
 
-export {renderComponent, expect};
+  TestUtils.Simulate[eventName](this[0]);
+}
+
+// 4. set up chai-jquery
+chaiJquery(chai, chai.util, $);
+
+
+// export
+export { renderComponent, expect };
