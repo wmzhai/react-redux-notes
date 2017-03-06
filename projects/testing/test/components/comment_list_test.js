7671d2cacbc69620b61bdf40021dd7ff6815dd96
Weiming Zhai <wmzhai@gmail.com>
2017-03-06 21:08:56 +0800

    expectation on contents

--- projects/testing/test/components/comment_list_test.js
+++ projects/testing/test/components/comment_list_test.js
@@ -2,7 +2,20 @@ import { renderComponent, expect } from '../test_helper';
 import CommentList from '../../src/components/comment_list';
 
 describe('CommentList', () => {
+  let component, props;
 
+  beforeEach( () => {
+    props = { comments: [ 'New Comment', 'Other New Comment' ]};
+    component = renderComponent(CommentList, null, props);
+  });
+
+  it('shows an li for each comment', () => {
+    expect(component.find('li').length).to.equal(2);
+  });
   
+  it('shows each comment that is provided', () => {
+    expect(component).to.contain(props.comments[0]);
+    expect(component).to.contain(props.comments[1]);
+  });
 
 });
