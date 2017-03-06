5c2498d1ecfbf16c7e4c53cf929e9e25cc4f336d
Weiming Zhai <wmzhai@gmail.com>
2017-03-06 22:24:20 +0800

    reducer tests

--- projects/testing/src/components/comment_box.js
+++ projects/testing/src/components/comment_box.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-export default class CommentBox extends Component {
+class CommentBox extends Component {
   constructor(props){
     super(props);
 
@@ -13,6 +15,8 @@ export default class CommentBox extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
     this.setState( { comment: ''});
   }
 
@@ -27,3 +31,6 @@ export default class CommentBox extends Component {
     )
   }
 }
+
+
+export default connect(null,actions)(CommentBox);
