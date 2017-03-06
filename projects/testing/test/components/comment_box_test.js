041d01a3766988bd6fb4d94ea0896520091e54a5
Weiming Zhai <wmzhai@gmail.com>
2017-03-06 20:41:51 +0800

    test controlled component

--- projects/testing/test/components/comment_box_test.js
+++ projects/testing/test/components/comment_box_test.js
@@ -3,7 +3,7 @@ import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
   let component;
-  
+
   beforeEach(() => {
     component = renderComponent(CommentBox);
   });
@@ -18,5 +18,19 @@ describe('CommentBox', () => {
 
   it('has a button', () => {
     expect(component.find('button')).to.exist;
+  });
+
+  describe('entering some text', () => {
+    beforeEach(()=>{
+      component.find('textarea').simulate('change','new comment');
+    });
+
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+
+    });
   });
 });
