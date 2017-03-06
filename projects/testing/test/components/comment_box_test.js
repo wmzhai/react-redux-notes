bd664689c4422d0f45fdb07abe5a7cc44941efac
Weiming Zhai <wmzhai@gmail.com>
2017-03-06 18:01:11 +0800

    to.have.class

--- projects/testing/test/components/comment_box_test.js
+++ projects/testing/test/components/comment_box_test.js
@@ -2,6 +2,11 @@ import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+  it('has the correct class', () => {
+    const component = renderComponent(CommentBox);
+    expect(component).to.have.class('comment-box');
+  });
+
   it('has a text area', () => {
     const component = renderComponent(CommentBox);
     expect(component.find('textarea')).to.exist;
@@ -10,5 +15,5 @@ describe('CommentBox', () => {
   it('has a button', () => {
     const component = renderComponent(CommentBox);
     expect(component.find('button')).to.exist;
-  })
+  });
 });
