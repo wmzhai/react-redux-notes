2fe1fc7a6bd7b738afe9d665f7307c9f9e9f15ac
Weiming Zhai <wmzhai@gmail.com>
2017-03-08 07:32:38 +0800

    jwtOptions

--- projects/server/services/passport.js
+++ projects/server/services/passport.js
@@ -5,18 +5,21 @@ const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // Setup options for JWT strategy
-const jwtOptions = {};
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
+  secrecOrKey: config.secret
+};
 
 
 // Create JWT strategy
 const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done){
   // 查看payload里的userid是否存在于数据库中，相应做处理
   User.findById(payload.sub, function(err, user){ //payload.sub就应该是userid
-    if(err) {return done(err,false); } 
+    if(err) {return done(err,false); } //查找出错
     if(user) {
-      done(null,user);
+      done(null,user); //查找正确，找到了
     } else {
-      done(null,false);
+      done(null,false); //查找正确，没找到
     }
   })
 });
