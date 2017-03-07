b6fda35a2468ee6068a56d97a0d2b99a10db3470
Weiming Zhai <wmzhai@gmail.com>
2017-03-07 15:11:46 +0800

    css

--- projects/middlewares/src/components/user_list.js
+++ projects/middlewares/src/components/user_list.js
@@ -20,7 +20,7 @@ class UserList extends Component {
 
   render() {
     return (
-      <div>
+      <div className="user-list">
         {this.props.users.map(this.renderUser)}
       </div>
     );
