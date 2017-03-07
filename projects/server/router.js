6f41ba8fe126bde432954f9120e42e2f0bbb8deb
Weiming Zhai <wmzhai@gmail.com>
2017-03-07 23:38:36 +0800

    Authentication Controller

--- projects/server/router.js
+++ projects/server/router.js
@@ -1,5 +1,6 @@
+const Authentication = require('./controllers/authentication');
+
 module.exports = function(app){
-  app.get('/', function(req, res, next){
-    res.send(['water', 'phone', 'paper']);
-  });
+  app.post('/signup', Authentication.signup);
+
 }
