20bf704a4aa924f3e09ae74f89eafb992a7f9c89
Weiming Zhai <wmzhai@gmail.com>
2017-03-07 11:16:14 +0800

    react router

---
+++ projects/hoc/src/components/resources.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <li>1 Cup Sugger</li>
+      <li>1 Cup Pepper</li>
+      <li>1 Cup Salt</li>
+    </div>
+  );
+}
