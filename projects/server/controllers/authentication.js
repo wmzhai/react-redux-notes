2aa89030209abf223fdf612f7ff42f9509566d99
Weiming Zhai <wmzhai@gmail.com>
2017-03-07 23:53:03 +0800

    implement signup

--- projects/server/controllers/authentication.js
+++ projects/server/controllers/authentication.js
@@ -1,3 +1,31 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({success: 'true'});
+  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // 查看是否存在这个email
+  User.findOne({ email: email }, function(err, existingUser){
+    if( err ) { return next(err); }
+
+    //如果存在，则返回error
+    if( existingUser ) {
+      return res.status(422).send({ error: 'Emails is in use' });
+    }
+  });
+
+  // 如果email不存在，则创建用户并保存到数据库
+  const user = new User({
+    email: email,
+    password: password
+  });
+
+  //保存到数据库
+  user.save(function(err){
+    if(err) {return next(err);}
+
+    //反馈成功信息
+    res.json(user);
+  }); 
 }
