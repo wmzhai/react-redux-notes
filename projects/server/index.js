989db8b8502c823ca6bb4e74d4161d3a30475f39
Weiming Zhai <wmzhai@gmail.com>
2017-03-07 22:22:24 +0800

    add middleware and nodemon

--- projects/server/index.js
+++ projects/server/index.js
@@ -2,11 +2,12 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const morgan = require('morgan'); 
 const app = express();
 
 // app setup
-
+app.use(morgan('combined'));
+app.use(bodyParser.json({type:'*/*'})); // parse incomming request
 
 // server setup
 const port = process.env.PORT || 8080;
