abf9b56783d8a36c446bce55c9a0c60d3556e49f
Weiming Zhai <wmzhai@gmail.com>
2017-03-08 16:14:08 +0800

    finish signout

---
+++ projects/client/src/components/auth/signout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
+
+class SignOut extends Component {
+  componentWillMount(){
+    this.props.signoutUser();
+  }
+
+  render() {
+    return <div> Bye... </div>;
+  }
+}
+
+export default connect(null, actions)(SignOut);
--- projects/client/src/components/header.js
+++ projects/client/src/components/header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     if( this.props.authenticated) {
       return(
         <li className="nav-item">
-          <Link className="nav-link">Sign Out</Link>
+          <Link className="nav-link" to="/signout">Sign Out</Link>
         </li>
       );
     } else {
