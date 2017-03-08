6dc81498064ddea48baab0927124fd50f6889a6d
Weiming Zhai <wmzhai@gmail.com>
2017-03-08 23:10:52 +0800

    automatic authenticate

--- projects/client/src/index.js
+++ projects/client/src/index.js
@@ -13,11 +13,18 @@ import Feature from './components/feature';
 import RequireAuth from './components/auth/require_auth';
 import Welcome from './components/welcome';
 import reducers from './reducers';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+if(token){
+  store.dispatch({type: AUTH_USER});
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App} > 
         <IndexRoute component={Welcome}/>
